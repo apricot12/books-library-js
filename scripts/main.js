501e2d197a95c27d277d0caa3d24b90039644582
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-05-11 19:52:11 +0500

    Add addingBook and displayLibrary functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,58 @@
-let library = []
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
-const pages = document.querySelector('#pages')
-const read = document.querySelector('#read')
-const crt_btn = document.querySelector('#create-btn')
-const show_books = document.querySelector('#show-books')
+let myLibrary = [];
 
-const Book = (title, author, pages, read) => {
-    const bTitle = () => title;
-    const bAuthor = () => author;
-    const bPages = () => pages;
-    const bRead = () => read;
-    const readStatus = () => read ? 'Book read' : 'Book not read'
-
+function Book(author, title, pages, read) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = read
 }
 
-function savLib() {
-    const holder = [];
-
-    for (let i = 0; i < library.length; i += 1) {
-        const bookInfo = [library[i].bTitle, library[i].bAuthor, library[i].bPages, library[i].bRead];
-        holder.push(bookInfo)
-    }
-    localStorage.setItem("SavedBooks", JSON.stringify(holder));
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
 }
+
+function displayLibrary(newBook) {
+    let table = document.querySelector('table');
+    let tr = document.createElement('tr');
+    tr.classList.add('book-row');
+
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let td5 = document.createElement('td');
+    let removeBtn = document.createElement('button');
+    let td4Btn = document.createElement('button');
+
+    td1.textContent = newBook.author;
+    td2.textContent = newBook.title;
+    td3.textContent = newBook.pages;
+    td4.textContent = newBook.read;
+    removeBtn.textContent = "Remove";
+    td4Btn.textContent = "Change the status";
+    td4Btn.classList.add("float-right");
+    tr.setAttribute("data-index", myLibrary.indexOf(newBook));
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    td5.appendChild(removeBtn);
+    td4.appendChild(td4Btn);
+
+    table.appendChild(tr);
+}
+
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+    let author = document.querySelector("#author");
+    let title = document.querySelector("#title");
+    let pages = document.querySelector("#pages");
+    let read = document.querySelector("#read");
+
+    let newBook = new Book(author.value, title.value, pages.value, read.checked);
+    addBookToLibrary(newBook);
+    displayLibrary(newBook);
+});
