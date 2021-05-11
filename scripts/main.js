94c82b3c3e1ec3c9ed5163fa6d758e3d24210a98
Nihal Mohammed <mnihal64@outlook.com>
2021-05-11 22:25:34 +0530

    Fix linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,67 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(author, title, pages, read) {
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.read = read
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
+  myLibrary.push(newBook);
 }
 
 function displayLibrary(newBook) {
-    let table = document.querySelector('table');
-    let tr = document.createElement('tr');
-    tr.classList.add('book-row');
+  const table = document.querySelector('table');
+  const tr = document.createElement('tr');
+  tr.classList.add('book-row');
 
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    let td5 = document.createElement('td');
-    let removeBtn = document.createElement('button');
-    let td4Btn = document.createElement('button');
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const td3 = document.createElement('td');
+  const td4 = document.createElement('td');
+  const td5 = document.createElement('td');
+  const removeBtn = document.createElement('button');
+  const td4Btn = document.createElement('button');
 
-    td1.textContent = newBook.author;
-    td2.textContent = newBook.title;
-    td3.textContent = newBook.pages;
-    td4Btn.textContent = newBook.read;
-    removeBtn.textContent = "Remove";
-    td4Btn.classList.add("float-right");
-    tr.setAttribute("data-index", myLibrary.indexOf(newBook));
+  td1.textContent = newBook.author;
+  td2.textContent = newBook.title;
+  td3.textContent = newBook.pages;
+  td4Btn.textContent = newBook.read;
+  removeBtn.textContent = 'Remove';
+  td4Btn.classList.add('float-right');
+  tr.setAttribute('data-index', myLibrary.indexOf(newBook));
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-    td5.appendChild(removeBtn);
-    td4.appendChild(td4Btn);
-    table.appendChild(tr);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  tr.appendChild(td5);
+  td5.appendChild(removeBtn);
+  td4.appendChild(td4Btn);
+  table.appendChild(tr);
 
-    td4Btn.addEventListener('click', () => {
-        if (td4Btn.textContent === "true") {
-            td4Btn.textContent = "false"
-        } else {
-            td4Btn.textContent = "true"
-        }
-    })
+  td4Btn.addEventListener('click', () => {
+    if (td4Btn.textContent === 'true') {
+      td4Btn.textContent = 'false';
+    } else {
+      td4Btn.textContent = 'true';
+    }
+  });
 
-    removeBtn.addEventListener('click', () => {
-        tr.remove();
-    })
-
+  removeBtn.addEventListener('click', () => {
+    tr.remove();
+  });
 }
 
-let btn = document.querySelector('button');
-btn.addEventListener('click', function() {
-    let author = document.querySelector("#author");
-    let title = document.querySelector("#title");
-    let pages = document.querySelector("#pages");
-    let read = document.querySelector("#read");
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  const author = document.querySelector('#author');
+  const title = document.querySelector('#title');
+  const pages = document.querySelector('#pages');
+  const read = document.querySelector('#read');
 
-    let newBook = new Book(author.value, title.value, pages.value, read.checked);
-    addBookToLibrary(newBook);
-    displayLibrary(newBook);
+  const newBook = new Book(author.value, title.value, pages.value, read.checked);
+  addBookToLibrary(newBook);
+  displayLibrary(newBook);
 });
